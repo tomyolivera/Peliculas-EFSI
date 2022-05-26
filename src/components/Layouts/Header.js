45191fe0bb27fbc4e55a488f79bb2f22f7d212b4
tomyolivera <tomasolivera27@gmail.com>
2022-05-26 18:21:51 -0300

    diseño

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -3,20 +3,12 @@ import { Navbar, NavbarBrand, NavLink } from "reactstrap";
 const Header = () => {
     return (
         <div>
-<<<<<<< HEAD
-            <Navbar color="dark" dark>
-=======
             <Navbar style={{ backgroundColor: '#182952' }}  dark>
->>>>>>> 2f36034 (diseño)
                 <NavbarBrand href="/">
                     Reactstrap
                 </NavbarBrand>
 
-<<<<<<< HEAD
-                <NavLink href="/peliculas">Peliculas</NavLink>
-=======
                 <NavLink style={{color:"white"}} href="/">Peliculas</NavLink>
->>>>>>> 2f36034 (diseño)
             </Navbar>
         </div>
     )
